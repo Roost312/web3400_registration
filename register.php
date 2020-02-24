b9df5316cfc32c6038f99ea0e29cb908423b93ea
ohhellwhipple <39172970+ohhellwhipple@users.noreply.github.com>
2020-02-24 11:00:19 -0700

    Add files via upload

--- register.php
+++ register.php
@@ -1,20 +1,6 @@
 <?php
 
-//db connection constants
-//db connection constants
-$DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'W01173226';
-$DATABASE_PASS = 'Jessencs!';
-$DATABASE_NAME = 'W01173226';
-
-// Try and connect using the info above.
-$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-
-if ( mysqli_connect_errno() ) {
-
-  // If there is an error with the connection, stop the script and display the error.
-die ('Failed to connect to MySQL: ' . mysqli_connect_error());
-}
+require 'dbcon.php';
 
 if (!isset($_POST['username'], $_POST['password'], $_POST['email'])) {
     die ('Please complete the registration form!');
